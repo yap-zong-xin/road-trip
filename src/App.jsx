b47ef17fd1016aea47096d1d4d91bdc480acb2e9
Yap Zong Xin <yapzongxin@hotmail.com>
2025-02-25 04:00:41 +0800

    add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { SearchProvider } from "./contexts/SearchContext";
+
 import CityList from "./components/CityList";
 import City from "./components/City";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { SearchProvider } from "./contexts/SearchContext";
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 export default function App() {
     return (
         <CitiesProvider>
             <SearchProvider>
                 <BrowserRouter>
-                    <Routes>
-                        <Route index element={<Homepage />} />
-                        <Route path="product" element={<Product />} />
-                        <Route path="pricing" element={<Pricing />} />
-                        <Route path="login" element={<Login />} />
-                        {/* nested route */}
-                        <Route path="app" element={<AppLayout />}>
-                            <Route
-                                index
-                                element={<Navigate replace to="cities" />}
-                            />
-                            <Route path="cities" element={<CityList />} />
-                            <Route path="cities/:id" element={<City />} />
-                            <Route path="countries" element={<CountryList />} />
-                            <Route path="form" element={<Form />} />
-                        </Route>
-                        <Route path="*" element={<PageNotFound />} />
-                    </Routes>
+                    <Suspense fallback={<SpinnerFullPage />}>
+                        <Routes>
+                            <Route index element={<Homepage />} />
+                            <Route path="product" element={<Product />} />
+                            <Route path="pricing" element={<Pricing />} />
+                            <Route path="login" element={<Login />} />
+                            {/* nested route */}
+                            <Route path="app" element={<AppLayout />}>
+                                <Route
+                                    index
+                                    element={<Navigate replace to="cities" />}
+                                />
+                                <Route path="cities" element={<CityList />} />
+                                <Route path="cities/:id" element={<City />} />
+                                <Route
+                                    path="countries"
+                                    element={<CountryList />}
+                                />
+                                <Route path="form" element={<Form />} />
+                            </Route>
+                            <Route path="*" element={<PageNotFound />} />
+                        </Routes>
+                    </Suspense>
                 </BrowserRouter>
             </SearchProvider>
         </CitiesProvider>
